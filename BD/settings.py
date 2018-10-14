7cce53d8e39f2b4c2be0d7aa2d60dff6807b9509
Loraine Monteagudo García <loraine.monteagudo@gmail.com>
2018-10-13 22:02:51 -0500

    Design and js functionality to add the careers

--- BD/settings.py
+++ BD/settings.py
@@ -77,17 +77,6 @@ WSGI_APPLICATION = 'BD.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),       
-        'ENGINE': 'django.db.backends.mysql',    #Add ’postgresql_psycopg2’, ’mysql’, ’sqlite3’ or ’oracle’.
-        'NAME': 'students',                         #Or path to database file if using sqlite3.
-        'USER': 'root',                         #Not used with sqlite3.
-        'PASSWORD': '1998',                     #Not used with sqlite3.
-        'HOST': 'localhost',                         #Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                         #Set to empty string for default. Not used with sqlite3.
-=======
     'default': {    
         'ENGINE': 'django.db.backends.mysql',    #Add ’postgresql_psycopg2’, ’mysql’, ’sqlite3’ or ’oracle’.
         'NAME': 'students',                         #Or path to database file if using sqlite3.
@@ -95,7 +84,6 @@ DATABASES = {
         'PASSWORD': '1998',                     #Not used with sqlite3.
         'HOST': 'localhost',                         #Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                        #Set to empty string for default. Not used with sqlite3.
->>>>>>> mysql
     }
 }
 
